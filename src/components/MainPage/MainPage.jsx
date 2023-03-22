262a43f640132d4ff3bc9796565d0c21ec61a5f6
sony-vedom <minerva.lankmiller@gmail.com>
2023-03-22 17:14:09 +0300

    Update MainPage.jsx

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,8 +1,16 @@
  import styles from "./MainPage.module.css"
 import Header from "./Header/Header";
 import Main from "./Content/Content";
+ import {useCookies} from 'react-cookie';
+ import {Navigate} from 'react-router-dom';
+
 
 const MainPage = () => {
+    const [cookies] = useCookies(["login"]);
+
+    if (cookies.login !== "true") {
+        return <Navigate to={`/login`}/>
+    }
     return (
         <div className={styles.frame}>
             <Header/>
